881701d9e46888a81913f0b1213669bafb2879f7
Selene <hakobyan.sn@gmail.com>
2015-07-27 22:49:28 -0700

    updated incoming_controller.rb

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -6,7 +6,28 @@ class IncomingController < ApplicationController
   def create
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.
-    puts "INCOMING PARAMS HERE: #{params}"
+    # puts "INCOMING PARAMS HERE: #{params}"
+
+    # Find the user by using params[:sender]
+
+    @user = User.find_by(email: params[sender])
+    
+    # Find the topic by using params[:subject]
+    @topic = Topic.find_by(user_id: @user, title: params[:subject])
+    
+    # Assign the url to a variable after retreiving it from params["body-plain"]
+    @url = "http://#{params["body-plain"]}"
+
+    # Check if user is nil, if so, create and save a new user
+    @user = User.create(email: params[:sender], password: "#{params[:title]_blocmarks}") if @user.nil?
+
+
+    # Check if the topic is nil, if so, create and save a new topic
+    @topic = Topic.new(user_id: @user, title: params[:title]) if @topic.nil?
+
+    # Now that you're sure you have a valid user and topic, build and save a new bookmark
+    @bookmark = Bookmark.create(Bookmark.setEmbedly(params['body-plain']))
+    @bookmark.update_attributes(topic_id: @topic_id)
 
     # You put the message-splitting and business
     # magic here. 
