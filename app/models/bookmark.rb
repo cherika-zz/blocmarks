8ac66d7a43efb372fe55329c4c9bff604afc86d6
Selene <hakobyan.sn@gmail.com>
2015-07-28 19:14:02 -0700

    incoming controller

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,11 +1,4 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :topic
-
-  def self.build_card(params)
-    bookmark = new(params)
-    thumbnail = ThumbnailService.new(params[:url])
-    bookmark.title = thumbnail.title
-    bookmark.thumbnail_url = thumbnail.url
-    bookmark
-  end
+  belongs_to :user
 end
