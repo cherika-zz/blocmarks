f172e736ca33c377c8b5e5bdf6beed8068ffa921
Selene <hakobyan.sn@gmail.com>
2015-08-09 00:48:53 -0700

    bookmarks crud

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,9 +2,60 @@ class BookmarksController < ApplicationController
   def index
     @bookmarks = Bookmark.all
   end
-  
-  def show
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @topic.user = current_user
+    @bookmarks = @topic.bookmarks
+
+    @bookmark = @topic.bookmarks.build(params.require(:bookmark).permit(:url))
+    @bookmark.user = current_user
+    if @bookmark.save
+      flash[:notice] = "Bookmark was saved successfully."
+      redirect_to @topic
+    else
+      flash[:notice] = "There was an error saving your bookmark. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @topic.user = current_user
+    @bookmarks = @topic.bookmarks
+
+    @bookmark = @topic.bookmarks.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @topic.user = current_user
+    @bookmarks = @topic.bookmarks
+
+    @bookmark = @topic.bookmarks.find(params[:id])
+    if @bookmark.update_attributes(params.require(:bookmark).permit(:url))
+      flash[:notice] = "Bookmark was updated successfully."
+      redirect_to @topic
+    else
+      flash[:notice] = "There was an error saving your request."
+      render :edit
+    end
+  end
+
+  def destroy
     @topic = Topic.find(params[:topic_id])
     @bookmark = @topic.bookmarks.find(params[:id])
+    # @bookmark.topic = @topic
+    if @bookmark.destroy
+      flash[:notice] = "Bookmark was deleted successfully."
+      redirect_to @topic
+    else
+      flash[:notice] = "There was an error. Please try again."
+      render :new
+    end
   end
 end
